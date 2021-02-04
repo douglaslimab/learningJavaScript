df6f47354b2779254e9512a1938355ab0524077d
Douglas Lima Benevides <51888076+douglaslimab@users.noreply.github.com>
2021-02-04 19:49:52 +0000

    first steps

---
+++ learningJS/004/js/script.js
@@ -0,0 +1,6 @@
+
+var name = window.prompt('What is your name?');
+document.write(`hello, ${name}. <br>`);
+document.write(`Your name has ${name.length} letters. <br>`);
+document.write(`Name: ${name.toUpperCase()} <br>`);
+document.write(`Name: ${name.toLowerCase()} <br>`);
